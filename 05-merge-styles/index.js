fdeb3830e0712c4382489db95c7c219e00eeb808
aliya <tusupkalievaaliya921@gmail.com>
2023-05-08 19:04:51 +0500

    add 05-merge-styles

---
+++ 05-merge-styles/index.js
@@ -0,0 +1,26 @@
+let fs = require('fs')
+let path = require('path')
+let bun = path.join(__dirname, 'project-dist', 'bundle.css')
+let css = path.join(__dirname, 'styles')
+fs.readdir(css, 'utf-8', function (error, files) {
+    if (error) {
+        throw error
+    }
+    fs.writeFile(bun, '', function (error) {
+        if (error) {
+            throw error
+        }
+    })
+    files.forEach(function (file) {
+        if (path.parse(path.join(css, file)).ext === '.css') {
+            let stream = fs.createReadStream(path.join(css, file))
+            stream.on('data', function (data) {
+                fs.appendFile(bun, data, function (error) {
+                    if (error) {
+                        throw error
+                    }
+                })
+            })
+        }
+    })
+})
